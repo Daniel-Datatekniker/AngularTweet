e4349bbdf8cf7fa28feac416acf2aa30d660d3e6
Daniel Sørensen <Daniel@2changeerp.dk>
2020-08-11 13:39:17 +0200

    master

---
+++ src/app/tweets/tweets.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { Tweet } from '../tweet';
+import { TweetsService } from '../tweets.service';
+import { NumberSymbol } from '@angular/common';
+import { debug } from 'console';
+
+@Component({
+  selector: 'app-tweets',
+  templateUrl: './tweets.component.html',
+  styleUrls: ['./tweets.component.css']
+})
+export class TweetsComponent implements OnInit {
+
+like = 0;
+searchText : string;
+
+  twat: Tweet[];
+  show : boolean= true;
+  constructor(tService: TweetsService) {
+       this.twat = tService.tweets;
+       console.log(this.twat.length);
+   }
+
+  ngOnInit() {
+  }
+
+  likes() {
+    this.like = (this.like)
+
+}
+save(text : string)
+{
+  this.searchText = text
+
+  
+}
+match(text : string){
+
+if(text.includes(this.searchText)){
+  
+  return true;
+}
+else if(this.searchText == undefined){
+  return true;
+}
+else{
+  return false;
+} 
+}
+
+}
